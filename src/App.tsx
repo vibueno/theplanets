40e6716bd4764aa305ac2139dce0ac57aabfd362
vibueno <20514260+vibueno@users.noreply.github.com>
2021-07-05 15:56:20 +0200

    Add font styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,13 @@ import React from 'react';
 
 import './index.scss';
 
-const App = () => <>React hello World!</>;
+const App = () => (
+  <>
+    <h1>I am an H1 header</h1>
+    <h2>I am an H2 header</h2>
+    <h3>I am an H3 header</h3>
+    <h4>I am an H4 header</h4>
+    <div>I am normal text</div>
+  </>
+);
 export default App;
