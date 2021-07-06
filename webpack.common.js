545104fbd5dfc35356d07b26011f397f8e74e84b
vibueno <20514260+vibueno@users.noreply.github.com>
2021-07-06 16:23:55 +0200

    Add background

--- webpack.common.js
+++ webpack.common.js
@@ -39,6 +39,17 @@ module.exports = {
             loader: 'file-loader'
           }
         ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
       }
     ]
   },
