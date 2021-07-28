896cf2a9fad05f33ab23f0b29f901ad39a17fe69
vibueno <20514260+vibueno@users.noreply.github.com>
2021-07-28 12:14:00 +0200

    Add geology picture

--- src/components/PlanetPic/index.tsx
+++ src/components/PlanetPic/index.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
 
+import { planetNames } from '../../constants';
+
 import './index.scss';
 
-type PlanetPicProps = {
-  planetName: string;
-  section: string;
-};
+import geologyMercury from '../../assets/img/geology-mercury.png';
+import geologyVenus from '../../assets/img/geology-venus.png';
+import geologyEarth from '../../assets/img/geology-earth.png';
+import geologyMars from '../../assets/img/geology-mars.png';
+import geologyJupiter from '../../assets/img/geology-jupiter.png';
+import geologySaturn from '../../assets/img/geology-saturn.png';
+import geologyUranus from '../../assets/img/geology-uranus.png';
+import geologyNeptune from '../../assets/img/geology-neptune.png';
 
 const getSectionSuffix = (section: string) => {
   if (section === 'structure') return '-internal';
   else return '';
+};
+
+const getGeologyPic = (planetName: string) => {
+  switch (planetName) {
+    case planetNames.mercury:
+      return geologyMercury;
+      break;
+    case planetNames.venus:
+      return geologyVenus;
+      break;
+    case planetNames.earth:
+      return geologyEarth;
+      break;
+    case planetNames.mars:
+      return geologyMars;
+      break;
+    case planetNames.jupiter:
+      return geologyJupiter;
+      break;
+    case planetNames.saturn:
+      return geologySaturn;
+      break;
+    case planetNames.uranus:
+      return geologyUranus;
+      break;
+    case planetNames.neptune:
+      return geologyNeptune;
+      break;
+    default:
+      break;
+  }
+};
+
+type PlanetPicProps = {
+  planetName: string;
+  section: string;
 };
 
 const PlanetPic = ({ planetName, section }: PlanetPicProps) => (
@@ -21,6 +63,9 @@ const PlanetPic = ({ planetName, section }: PlanetPicProps) => (
       )}.svg`)}
       alt={planetName}
     />
+    {section === 'geology' ? (
+      <img src={getGeologyPic(planetName)} alt={planetName} />
+    ) : null}
   </>
 );
 
---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+const planetNames = {
+  mercury: 'mercury',
+  venus: 'venus',
+  earth: 'earth',
+  mars: 'mars',
+  jupiter: 'jupiter',
+  saturn: 'saturn',
+  uranus: 'uranus',
+  neptune: 'neptune'
+};
+
+export { planetNames };
