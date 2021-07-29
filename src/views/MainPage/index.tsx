7d4a95a2f66f5c61225a5c097f22413ebc22c0f2
vibueno <20514260+vibueno@users.noreply.github.com>
2021-07-29 14:29:09 +0200

    Planet pics ratio

--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -51,11 +51,6 @@ const MainPage = () => {
               planet={planet}
               clickHandler={buttonPanelclickHandler}
             />
-            <h1>I am an H1 header</h1>
-            <h2>I am an H2 header</h2>
-            <h3>I am an H3 header</h3>
-            <h4>I am an H4 header</h4>
-            <div>I am normal text</div>
           </div>
           <div className="planet-data-container">
             <PlanetData />
