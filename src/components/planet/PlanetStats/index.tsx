103042d01af3b9961c3efc1211921fe24810a856
vibueno <20514260+vibueno@users.noreply.github.com>
2022-02-21 11:52:48 +0100

    Fix responsiveness at 1366x768px

--- src/components/planet/PlanetStats/index.tsx
+++ src/components/planet/PlanetStats/index.tsx
@@ -10,21 +10,32 @@ type PlanetStatsProps = {
   currentPlanetKey: string;
 };
 
-const PlanetStats = ({ currentPlanetKey }: PlanetStatsProps) => (
-  <>
+const PlanetStats = ({ currentPlanetKey }: PlanetStatsProps) => {
+  const planetStatsItems = [];
+
+  for (let index = 0; index < STATS_KEYS.length; index++) {
+    planetStatsItems.push(
+      <PlanetStatsItem
+        key={STATS_KEYS[index]}
+        title={STATS[STATS_KEYS[index]].TITLE}
+        content={getPlanetData(
+          PLANETS[currentPlanetKey].NAME,
+          STATS[STATS_KEYS[index]].NAME
+        )}
+      />
+    );
+  }
+
+  return (
     <div className={styles.planetStats}>
-      {STATS_KEYS.map(statsKey => (
-        <PlanetStatsItem
-          key={statsKey}
-          title={STATS[statsKey].TITLE}
-          content={getPlanetData(
-            PLANETS[currentPlanetKey].NAME,
-            STATS[statsKey].NAME
-          )}
-        />
-      ))}
+      <div className={styles.planetStatsColumn}>
+        {planetStatsItems.slice(0, 2)}
+      </div>
+      <div className={styles.planetStatsColumn}>
+        {planetStatsItems.slice(2, 4)}
+      </div>
     </div>
-  </>
-);
+  );
+};
 
 export default PlanetStats;
